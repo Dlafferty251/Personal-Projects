010cd68c377a71f333a966c4495f160f5fe4cf10
Dlafferty251 <dlafferty251@gmail.com>
2025-03-18 15:32:11 +0000

    Feat/Adds Basic HTML and a few contents in Array

---
+++ src/reviewProject/main.js
@@ -0,0 +1,20 @@
+const reviewers = [
+{
+    id: 1,
+    name: "Brad Pitt",
+    job: "Actor",
+    Text: "Lorem Lipsum is a text to fill in used space",
+
+}, {
+    id: 2,
+    name: "Patrick Star",
+    job: "Stay At Home Starfish",
+    text: "Patrick starfish is a starfish that lives under a rock. He is Sponegebobs best friend.",
+}, {
+    id: 3,
+    name: "Dylan Lafferty",
+    job: "Line Cook",
+    text: "Dylan is a line cook at Mcglynns bar and pub."
+
+}
+];
