ce9a1920421868194047dce89a059971f80e2234
Dlafferty251 <dlafferty251@gmail.com>
2025-04-09 15:15:06 +0000

    Feat: Enhance Budgeting application with user input handling for income and expenses

--- src/BudgetingApp/app/BudgetSystem.java
+++ src/BudgetingApp/app/BudgetSystem.java
@@ -1,17 +1,25 @@
 package BudgetingApp.app;
+
+import java.util.Scanner;
+
 public class BudgetSystem {
     
     public static void main (String[] args) {
+
+        //Creates a new Scanner object that will read user input
+        Scanner scanner = new Scanner(System.in); 
+
         // This is the main method that starts the Budgeting application
         System.out.println("Welcome to the Budget Buddy!");
 
         // Get User Income
-        System.out.println(getUserIncome.getIncome());
-        // Get User Income
+        System.out.println(getUserIncome.getIncome(scanner));
 
         // Get User Expenses
+        String expenseResult = getUserExpenses.getExpenses(scanner);
+        System.out.println(expenseResult);
 
-
+        scanner.close();
 
         // Calculate Budget
 
--- src/BudgetingApp/app/getUserIncome.java
+++ src/BudgetingApp/app/getUserIncome.java
@@ -4,14 +4,16 @@ import java.util.Scanner;
 
 public class getUserIncome {
 
-    public static String getIncome() {
+    public static String getIncome(Scanner scanner) {
         //This method will get the user's income 
         //and return it as a string 
 
-        try (Scanner scanner = new Scanner(System.in)) {
+        try {
             System.out.println("Please enter your income for the month: ");
             String income = scanner.nextLine();
             return income;
+        } catch (Exception e) {
+            return "Your input was not valid. Please try again.";
         }
     }
 }
